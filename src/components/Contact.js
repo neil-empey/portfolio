bd070c38df5668dd5fb7cb2c39a0c9fef7a669a9
Neil E <argus.two.2@protonmail.com>
2021-08-16 00:11:20 -0700

    form test 4

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -35,8 +35,7 @@ export default function contact() {
                                     <div class="d-none" id="submitSuccessMessage">
                                         <div class="text-center mb-3">
                                             <div class="fw-bolder">Form submission successful!</div>
-                                            <p>To activate this form, sign up at</p>
-                                            <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
+                                            <p><strong>SUCCESS</strong></p>
                                         </div>
                                     </div>
                                     <div class="d-none" id="submitErrorMessage">
